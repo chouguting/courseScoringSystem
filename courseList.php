1923f066d301083f66684324defbdd9066de5f0d
chouguting <chouguting@gmail.com>
2021-12-26 00:49:47 +0800

    修改查詢

--- courseList.php
+++ courseList.php
@@ -1,11 +1,13 @@
-
 <?php
-include('header.php');?>
-
+include('header.php'); ?>
+<?php
+include_once "db_conn.php"; ?>
 <header class="mdc-top-app-bar">
     <div class="mdc-top-app-bar__row">
         <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-            <button class="material-icons mdc-top-app-bar__action-item mdc-icon-button"  onclick=location.href="index.php">arrow_back</button>
+            <button class="material-icons mdc-top-app-bar__action-item mdc-icon-button"
+                    onclick=location.href="index.php">arrow_back
+            </button>
             <span class="mdc-top-app-bar__title">課程列表</span>
         </section>
         <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
@@ -19,21 +21,18 @@ include('header.php');?>
     <br/>
 
 
-
-        <form action="inform.php" method="get">
+    <form action="inform.php" method="get">
         <h3> 查詢資料</h3>
-        <?php
-            include_once "db_conn.php";
 
-            echo "<table border='1'>
-                    <tr>
-                        <th>id</th>
-                        <th>username</th>
-                        <th>display_name</th>
-                        <th>password</th>
-                        <th>userLevel</th>
-                    </tr>
-                    ";
+        <table border='1' style='width:70%'>
+            <tr>
+                <th>id</th>
+                <th>username</th>
+                <th>display_name</th>
+                <th>password</th>
+                <th>userLevel</th>
+            </tr>
+            <?php
 
             $query = ("select * from user");
             $stmt = $db->prepare($query);
@@ -49,11 +48,12 @@ include('header.php');?>
                 echo "<td>" . $result[$i]['user_level'] . "</td>";
                 echo "</tr>";
             }
+            ?>
+        </table>
+        <br>
+        <input typut type='button' onclick='history.back()' value='Go back'></input>
 
-            echo "</table>";
-            echo "<br><input typut type='button' onclick='history.back()' value='Go back'></input>";
-        ?>
-    <a class="waves-effect waves-light btn-small margin5" onclick=location.href="inform.php">所有課程</a>
+        <a class="waves-effect waves-light btn-small margin5" onclick='history.back()'>所有課程</a>
 
 </center>
 
