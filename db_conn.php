39f7867fcbf1d2a175e76f0dd0a5fec548c2b0e4
Gordon Chou <chouguting@gmail.com>
2022-01-05 17:37:23 +0800

    add azure db

--- db_conn.php
+++ db_conn.php
@@ -1,9 +1,9 @@
 <?php
-    $user='root';
+    $user='root2';
     $password='20010122';
 
     try{
-        $db = new PDO('mysql:host=localhost;dbname=coursescoringsystem;charset=utf8',$user,$password);
+        $db = new PDO('mysql:host=course-scoring-db.mariadb.database.azure.com;dbname=coursescoringsystem;charset=utf8',$user,$password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }catch (PDOException $e){
